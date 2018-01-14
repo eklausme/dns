ffaab5b116ab8738d577cfccc24b4b45b6e36af5
Elmar Klausmeier <Elmar.Klausmeier@gmail.com>
2018-01-14 13:59:29 +0100

    Create eklausmeier.tk.lua
    Initial revision
    
    Used
        https://github.com/luadns/dns/blob/master/example.net.lua
    as guide.

---
+++ eklausmeier.tk.lua
@@ -0,0 +1,10 @@
+-- File: eklausmeier.tk.lua
+-- Zone: eklausmeier.tk
+-- Variable _a is replaced with zone name
+-- _a = "eklausmeier.tk"
+
+-- A record
+a(_a, "178.200.54.155")
+
+-- CNAME records
+cname("www", _a)
