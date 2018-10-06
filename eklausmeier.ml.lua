b1cfe653241a8123df4ac05fd64c34deaa6ffaf8
Elmar Klausmeier <Elmar.Klausmeier@gmail.com>
2018-10-06 20:32:38 +0200

    Initial revision

---
+++ eklausmeier.ml.lua
@@ -0,0 +1,10 @@
+-- File: eklausmeier.ml.lua
+-- Zone: eklausmeier.ml
+-- Variable _a is replaced with zone name
+-- _a = "eklausmeier.ml"
+
+-- A record
+a(_a, "88.152.176.16")
+
+-- CNAME records
+cname("www", _a)
